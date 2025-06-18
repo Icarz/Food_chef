d850b8120de81eb49c6f305ad808af63fa281c42
Icarz <mr.rghay@email.com>
2025-06-18 13:26:39 +0100

    looking fine

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 import Search from "./Components/Search";
 import FoodList from "./Components/FoodList";
 import NavBar from "./Components/NavBar";
+import Container from "./Components/Container";
 import "./App.css";
+import InnerContainer from "./Components/InnerContainer";
 const App = () => {
   const [foodData, setFoodData] = useState([]);
   return (
     <div>
       <NavBar />
       <Search fooData={foodData} setFoodData={setFoodData} />
-
-      <FoodList foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} setFoodData={setFoodData} />
+        </InnerContainer>
+      </Container>
     </div>
   );
 };
